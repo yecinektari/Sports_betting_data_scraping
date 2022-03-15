423fa7079e3cc76ca06f6b73ad442d25c131269d
Thomas Bouiniere <thomas.bouiniere@student-cs.fr>
2022-03-15 23:19:41 +0100

    nom equipe

--- winamax.py
+++ winamax.py
@@ -22,13 +22,29 @@ browser = webdriver.Chrome(
 
 browser.get('https://www.winamax.fr/paris-sportifs/sports/1/1/1')
 time.sleep(1)
+
 '''On récupère les cotes'''
+
 liens = browser.find_elements_by_xpath(
     '//*[@id="app"]/div[1]/div[1]/div/div[2]/div/section/div[3]/div[1]/div/div/div/div/div[1]/a')
-for lien in liens:
-    for i in range(0, 3):
-        x = lien.get_property('lastChild').get_property('lastChild').get_property('lastChild').get_property('children')[
-            0].get_property('children')[i].get_property('children')[1].get_property('children')[0].get_property('children')[1]
-        print(x.text)
+cotes_pl = []
+try:
+    for lien in liens:
+        for i in range(0, 3):
+            x = lien.get_property('lastChild')
+            x = x.get_property('lastChild')
+            x = x.get_property('lastChild')
+            x = x.get_property('children')[0]
+            x = x.get_property('children')[i]
+            x = x.get_property('children')[1]
+            x = x.get_property('children')[0]
+            x = x.get_property('children')[1]
+            cotes_pl.append(x.text)
+except:
+    None
+
 
 '''On récupère les noms d'équipes'''
+# noms_pl = browser.find_elements_by_xpath(
+#     '//*[@id="app"]/div[1]/div[1]/div/div[2]/div/section/div[3]/div[1]/div/div/div/div/div[1]/a/div[2]/div[1]/div[1]/div[1]/div/span')
+# print(noms_pl)
